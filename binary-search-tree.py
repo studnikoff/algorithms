3888e86aa1383f5c1263090b6824a34dffe9d592
ds <cfkfl1245@gmail.com>
2022-12-12 17:19:31 +0300

    feat: Binary search tree
    Binary Search Tree implementation (BST). It uses nodes with
    refs to left and right node (children). DFS algorithm for traversal.
    put, get, size, rank, ceiling and floor methods using recursion.

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -5,17 +5,21 @@ class Node:
     key = None
     value = None
     left_node, right_node = None, None
+    count = 0
 
     def __init__(self, key: int = None, value: Any = None) -> None:
         self.key = key
         self.value = value
+        if self.key is not None:
+            self.count = 1
 
     def __repr__(self):
         d = {
             "key": self.key,
             "value": self.value,
             "left_node": self.left_node,
-            "right_node": self.right_node
+            "right_node": self.right_node,
+            "count": self.count,
         }
         res = dict((k,v) for k,v in d.items() if v is not None)
         return str(res)
@@ -37,6 +41,7 @@ class BST:
             node.right_node = self.__put(node.right_node, key, value)
         else:
             node.value = value
+        node.count = 1 + self.__size(node.left_node) + self.__size(node.right_node)
         return node
 
     def get(self, key: int):
@@ -51,6 +56,110 @@ class BST:
             return self.__get(node.right_node, key)
         else:
             return node.value
+
+    def floor(self, key: int):
+        node = self.__floor(self.root, key)
+        if node is None:
+            return None
+        else:
+            return node.key
+
+    def __floor(self, node: Node, key: int) -> Node:
+        if node is None:
+            return None
+        elif node.key == key:
+            return node
+        elif (node.key > key):
+            return self.__floor(node.left_node, key)
+        else:
+            t = self.__floor(node.right_node, key)
+            if t is None:
+                return node
+            else:
+                return t
+
+    def ceiling(self, key: int):
+        node = self.__ceiling(self.root, key)
+        if node is None:
+            return None
+        else:
+            return node.key
+
+    def __ceiling(self, node: Node, key: int):
+        if node is None:
+            return None
+        elif node.key == key:
+            return node
+        elif node.key > key:
+            t = self.__ceiling(node.left_node, key)
+            if t is None:
+                return node
+            else:
+                return t
+        else:
+            return self.__ceiling(node.right_node, key)
+
+    def size(self):
+        return self.__size(self.root)
+
+    def __size(self, node: Node):
+        if node is None:
+            return 0
+        else:
+            return node.count
+
+    def rank(self, key: int):
+        return self.__rank(self.root, key)
+
+    def __rank(self, node: Node, key:int):
+        if node is None:
+            return 0
+        elif node.key > key:
+            return self.__rank(node.left_node, key)
+        elif node.key < key:
+            return 1 + self.__size(node.left_node) + self.__rank(node.right_node, key)
+        else:
+            return self.__size(node.left_node)
+
+    def inorder(self) -> list:
+        queue = list()
+        self.__inorder(self.root, queue)
+        return queue
+
+    def __inorder(self, node: Node, queue: list):
+        if node is None:
+            pass
+        else:
+            self.__inorder(node.left_node, queue)
+            queue.append(node.key)
+            self.__inorder(node.right_node, queue)
+
+    def preorder(self):
+        q = list()
+        self.__preorder(self.root, q)
+        return q
+
+    def __preorder(self, node: Node, queue: list):
+        if node is None:
+            pass
+        else:
+            queue.append(node.key)
+            self.__preorder(node.left_node, queue)
+            self.__preorder(node.right_node, queue)
+
+    def postorder(self):
+        q = list()
+        self.__postorder(self.root, q)
+        return q
+
+    def __postorder(self, node: Node, queue: list):
+        if node is None:
+            pass
+        else:
+            self.__preorder(node.left_node, queue)
+            self.__preorder(node.right_node, queue)
+            queue.append(node.key)
+
 
 
 if __name__ == '__main__':
@@ -73,4 +182,25 @@ if __name__ == '__main__':
     print(bst.root)
     print(bst.get(3))
 
+    print(bst.floor(7))
+    print(bst.floor(2))
+    print(bst.floor(4))
 
+    print("<==================>")
+
+    print(bst.ceiling(1))
+    print(bst.ceiling(2))
+    print(bst.ceiling(4))
+    print(bst.ceiling(7))
+
+    print(bst.root)
+
+    print(bst.rank(5))
+    print(bst.rank(6))
+    print(bst.rank(7))
+    print(bst.rank(1))
+    
+    # DFS depth first search
+    print(bst.inorder())
+    print(bst.preorder())
+    print(bst.postorder())
